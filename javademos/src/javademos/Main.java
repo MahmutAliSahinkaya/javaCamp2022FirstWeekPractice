c9a8afe8e36369761aa9088ff1584496053c128b
Mahmut Ali Şahinkaya <107810135+MahmutAliSahinkaya@users.noreply.github.com>
2022-09-16 12:02:19 +0300

    Add files via upload

---
+++ javademos/src/javademos/Main.java
@@ -0,0 +1,13 @@
+package javademos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Java Dersi 5 : Hello World Uygulaması
+		
+		System.out.println("Merhaba Java");
+		System.out.println("Merhaba" + " Java" + " 2");
+
+	}
+
+}
---
+++ variables/src/variables/Main.java
@@ -0,0 +1,22 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Java Dersi 6 : Değişkenler ve Değişken Kullanım İhtiyacı
+
+		// case sensitive -
+		// camelCasing
+
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+
+	}
+
+}
